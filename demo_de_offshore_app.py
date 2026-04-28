d3e60de2b998a66a0f8bf42a6dab4115105fae98
gitsome-82 <none>
2026-04-28 14:24:37 +0100

    added stupid launcher file to laumch the germany demo app to see if thats why stupid streamlit was being stupid

---
+++ demo_de_offshore_app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import runpy
+
+
+TARGET_APP = (
+    Path(__file__).resolve().parent
+    / "2. generation and load, scaling"
+    / "demo DE offshore gen vs load streamlit dash.py"
+)
+
+runpy.run_path(str(TARGET_APP), run_name="__main__")
